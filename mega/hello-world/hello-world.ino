b65821d03608375c5b3878942a10fa4c4d6a913b
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-02-14 20:07:33 +0800

    testing a quick hello world program

---
+++ mega/hello-world/hello-world.ino
@@ -0,0 +1,13 @@
+void setup(){
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    digitalWrite(13, HIGH);
+    Serial.print("Hello ");
+    delay(1000);
+    digitalWrite(13, LOW);
+    Serial.println("World! ");
+    delay(100);
+}
