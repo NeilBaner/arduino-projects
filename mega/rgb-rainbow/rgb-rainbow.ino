fa6cc7b9b673de3af1306c225d70957eb7aed6c7
NeilBaner <30567828+NeilBaner@users.noreply.github.com>
2020-02-18 14:55:05 +0800

    i'm an idiot who doesn't know how arrays work

--- mega/rgb-rainbow/rgb-rainbow.ino
+++ mega/rgb-rainbow/rgb-rainbow.ino
@@ -11,120 +11,120 @@
 #define BLUE_3 12
 #define BLUE_4 13
 
-static int colours[1536][3];
+static byte colours[1536][3];
 
-void setup(){
-    for(int i = 2; i <= 13; i++){
+void setup() {
+    for (int i = 2; i <= 13; i++) {
         pinMode(i, OUTPUT);
     }
 
     int j = 0;
-    for(int i = 255; j <= 255; j++){
+    for (int i = 255; j <= 255; j++) {
         colours[j][0] = i;
     }
-    for(int i = 255; j <= 512; j++){
+    for (int i = 255; j <= 511; j++) {
         colours[j][0] = i;
     }
-    for(int i = 255; j <= 768; i--, j++){
+    for (int i = 255; j <= 767; i--, j++) {
         colours[j][0] = i;
     }
-    for(int i = 0; j <= 1024; j++){
+    for (int i = 0; j <= 1023; j++) {
         colours[j][0] = i;
     }
-    for(int i = 0; j <= 1280; j++){
+    for (int i = 0; j <= 1279; j++) {
         colours[j][0] = i;
     }
-    for(int i = 0; j <= 1536; i++, j++){
+    for (int i = 0; j <= 1535; i++, j++) {
         colours[j][0] = i;
     }
 
     j = 0;
-    for(int i = 0; j <= 255; j++){
+    for (int i = 0; j <= 255; j++) {
         colours[j][1] = i;
     }
-    for(int i = 0; j <= 512; i++, j++){
+    for (int i = 0; j <= 511; i++, j++) {
         colours[j][1] = i;
     }
-    for(int i = 255; j <= 768; j++){
+    for (int i = 255; j <= 767; j++) {
         colours[j][1] = i;
     }
-    for(int i = 255; j <= 1024; j++){
+    for (int i = 255; j <= 1023; j++) {
         colours[j][1] = i;
     }
-    for(int i = 255; j <= 1280; i--, j++){
+    for (int i = 255; j <= 1279; i--, j++) {
         colours[j][1] = i;
     }
-    for(int i = 0; j <= 1536; j++){
+    for (int i = 0; j <= 1535; j++) {
         colours[j][1] = i;
     }
 
     j = 0;
-    for(int i = 255; j <= 255; i--, j++){
+    for (int i = 255; j <= 255; i--, j++) {
         colours[j][2] = i;
     }
-    for(int i = 0; j <= 512; j++){
+    for (int i = 0; j <= 511; j++) {
         colours[j][2] = i;
     }
-    for(int i = 0; j <= 768; j++){
+    for (int i = 0; j <= 767; j++) {
         colours[j][2] = i;
     }
-    for(int i = 0; j <= 1024; i++, j++){
+    for (int i = 0; j <= 1023; i++, j++) {
         colours[j][2] = i;
     }
-    for(int i = 255; j <= 1280; j++){
+    for (int i = 255; j <= 1279; j++) {
         colours[j][2] = i;
     }
-    for(int i = 255; j <= 1536; j++){
+    for (int i = 255; j <= 1535; j++) {
         colours[j][2] = i;
     }
 }
 
-void rainbow_one_led(int delayTime){
-    for(int i = 255; i >= 0; i--){
+void rainbow_one_led(int delayTime) {
+    for (int i = 255; i >= 0; i--) {
         analogWrite(BLUE_1, i);
         delay(delayTime);
     }
-    for(int i = 0; i <= 255; i++){
+    for (int i = 0; i <= 255; i++) {
         analogWrite(GREEN_1, i);
         delay(delayTime);
     }
-    for(int i = 255; i >= 0; i--){
+    for (int i = 255; i >= 0; i--) {
         analogWrite(RED_1, i);
         delay(delayTime);
     }
-    for(int i = 0; i <= 255; i++){
+    for (int i = 0; i <= 255; i++) {
         analogWrite(BLUE_1, i);
         delay(delayTime);
     }
-    for(int i = 255; i >= 0; i--){
+    for (int i = 255; i >= 0; i--) {
         analogWrite(GREEN_1, i);
         delay(delayTime);
     }
-    for(int i = 0; i <= 255; i++){
+    for (int i = 0; i <= 255; i++) {
         analogWrite(RED_1, i);
         delay(delayTime);
     }
 }
 
-void loop(){
-    int ledColour[4];
-    for(int i = 0; i < 1536; i++){
+void loop() {
+    short int ledColour[4];
+    for (int i = 0; i < 1536; i++) {
         ledColour[0] = i;
         ledColour[1] = (i + 384) < 1536 ? i + 384 : i + 384 - 1536;
         ledColour[2] = (i + 768) < 1536 ? i + 768 : i + 768 - 1536;
         ledColour[3] = (i + 1152) < 1536 ? i + 1152 : i + 1152 - 1536;
+        analogWrite(RED_1, colours[ledColour[0]][0]);
+        analogWrite(GREEN_1, colours[ledColour[0]][1]);
+        analogWrite(BLUE_1, colours[ledColour[0]][2]);
+        analogWrite(RED_2, colours[ledColour[1]][0]);
+        analogWrite(GREEN_2, colours[ledColour[1]][1]);
+        analogWrite(BLUE_2, colours[ledColour[1]][2]);
+        analogWrite(RED_3, colours[ledColour[2]][0]);
+        analogWrite(GREEN_3, colours[ledColour[2]][1]);
+        analogWrite(BLUE_3, colours[ledColour[2]][2]);
+        analogWrite(RED_4, colours[ledColour[3]][0]);
+        analogWrite(GREEN_4, colours[ledColour[3]][1]);
+        analogWrite(BLUE_4, colours[ledColour[3]][2]);
+        delay(1);
     }
-    analogWrite(RED_1, colours[ledColour[0], 0]);
-    analogWrite(GREEN_1, colours[ledColour[0], 1]);
-    analogWrite(BLUE_1, colours[ledColour[0], 2]);
-    analogWrite(RED_2, colours[ledColour[1], 0]);
-    analogWrite(GREEN_2, colours[ledColour[1], 1]);
-    analogWrite(BLUE_2, colours[ledColour[1], 2]);
-    analogWrite(RED_3, colours[ledColour[2], 0]);
-    analogWrite(GREEN_3, colours[ledColour[2], 1]);
-    analogWrite(BLUE_3, colours[ledColour[2], 2]);
-    analogWrite(RED_4, colours[ledColour[3], 0]);
-    analogWrite(GREEN_4, colours[ledColour[3], 1]);
-    analogWrite(BLUE_4, colours[ledColour[3], 2]);
-    delay(10);
 }
